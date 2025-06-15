458e40b8b8edde473decf99ff46d6cae220dc763
Wu Junyi <3049118371@qq.com>
2025-06-15 22:13:54 +0800

    level1 再回到room再回到level2

--- Assets/Scripts/DialogueEndFade.cs
+++ Assets/Scripts/DialogueEndFade.cs
@@ -6,14 +6,18 @@ public class DialogueEndFade : MonoBehaviour
     public DialogueRunner dialogueRunner;
     public ScreenFader screenFader;
 
+    public RoomMovement roomMovement;
+
     void Start()
     {
         dialogueRunner.onDialogueComplete.AddListener(OnDialogueFinished);
         screenFader.StartBlack(); // 一开始黑屏
+        roomMovement.enabled = false;
     }
 
     void OnDialogueFinished()
     {
         StartCoroutine(screenFader.FadeIn());
+        roomMovement.enabled = true;
     }
 }
